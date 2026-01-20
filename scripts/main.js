58572fe757dde4eaf440f86f80485bffb64271aa
Liquedator <kieranisnotcool@icloud.com>
2026-01-20 20:05:38 +1100

    - added webp variants to all images to be served as default to help with load times, pngs kept to be served as backup should the browser not support webp. - no more reliance on switching out images on hover, now just use the style.filter = brightness() function. - contact email copy button on click now runs off js timeout

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
+let revertTimer = null;
 function copyToClipboard() {
     const email = "mrdoily.band@gmail.com";
-    navigator.clipboard.writeText(email);
-    document.getElementById("contactEmail").src="../img/buttons/copied-pressed.png";
+    const img = document.getElementById("contactEmail");
+    const src = document.getElementById("emailSource");
+
+    if (revertTimer) {
+        clearTimeout(revertTimer);
+        revertTimer = null;
+    }
+
+    navigator.clipboard.writeText(email).then(() => {
+
+        src.srcset = "../img/buttons/copied.webp";
+        img.src = "../img/buttons/copied.png"
+        revertTimer = setTimeout(() => {
+            src.srcset = "../img/buttons/email.webp"
+            img.src = "../img/buttons/email.png"
+            revertTimer = null;
+        }, 1500);
+    });
 }
